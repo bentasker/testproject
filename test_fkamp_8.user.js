b83475a95955b37c29a9ee84a34cb6ac750080fd
B Tasker <github@bentasker.co.uk>
2019-06-13 16:14:15 +0100

    Add script for me to add to Greasemonkey

---
+++ test_fkamp_8.user.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name           Test script for FKAMP-8
+// @namespace      http
+// @description    Used to test whether updates work via Github
+// @include        *://*google*/search*
+// @include        *://news.google*/*
+// @run-at document-end
+// @grant none
+// @downloadURL https://github.com/bentasker/testproject/raw/master/test_fkamp_8.user.js
+// @version 1.4.21
+//
+//
+// See https://projects.bentasker.co.uk/jira_projects/browse/FKAMP-8.html
+//
+// Author: B Tasker
+// ==/UserScript==
+
+
+console.log("Loaded");
+
